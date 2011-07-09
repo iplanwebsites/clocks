075241d7a678c66096cef8e81eea843e88119b80
I Plan Websites <info@iplanwebsites.com>
2011-07-10 00:22:27 +0200

    efficiently parse json datafile to retrieve autocomplete txt bits

--- public/clock.js
+++ public/clock.js
@@ -2,3 +2,53 @@ function foo() {
 	document.getElementById('clock').textContent = new Date();
 	setTimeout(foo, 100);
 }
+
+function removeDuplicateElement(arrayName)
+  {
+  var newArray=new Array();
+  label:for(var i=0; i<arrayName.length;i++ )
+  {  
+  for(var j=0; j<newArray.length;j++ )
+  {
+  if(newArray[j]==arrayName[i]) 
+  continue label;
+  }
+  newArray[newArray.length] = arrayName[i];
+  }
+  return newArray;
+}
+
+
+$(document).ready(function() {
+	
+	
+	//sammy put route for the submit...
+	
+	
+	
+	//Load Json - init...
+	$.getJSON('data/cities.json', function(data) { //cached...
+	  var autocompleteItem = [];
+	  $.each(data, function(key, val) {
+	    autocompleteItem.push(val['ci']); //city name
+			autocompleteItem.push(val['z']); //zone name
+	  });
+
+		autocompleteItem = removeDuplicateElement(autocompleteItem);
+		
+// We init the auto-complete once we gathered all city + zones names.
+$("input#autocomplete").autocomplete({
+	source: autocompleteItem,
+	minLength: 1,
+	delay: 0
+});
+
+	});
+	
+	
+    
+		
+		
+		
+		
+});
