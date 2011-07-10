60fed00c2c0830acb17bf083eab1ab761e66c6c5
I Plan Websites <info@iplanwebsites.com>
2011-07-10 19:29:24 +0200

    Added Sammy.js, and a PUT route to add a new city.

--- public/clock.js
+++ public/clock.js
@@ -28,27 +28,78 @@ $(document).ready(function() {
 	
 	//Load Json - init...
 	$.getJSON('data/cities.json', function(data) { //cached...
-	  var autocompleteItem = [];
+		sammy.cities = data;  
+		var autocompleteItem = [];
 	  $.each(data, function(key, val) {
 	    autocompleteItem.push(val['ci']); //city name
 			autocompleteItem.push(val['z']); //zone name
 	  });
 
+		
 		autocompleteItem = removeDuplicateElement(autocompleteItem);
 		
 // We init the auto-complete once we gathered all city + zones names.
-$("input#autocomplete").autocomplete({
+$("input#q").autocomplete({
 	source: autocompleteItem,
 	minLength: 1,
 	delay: 0
 });
 
-	});
+	});// eo get Json (init)
 	
 	
+	
+	
+	
+	sammy = Sammy('body', function () {
+		this.use(Sammy.Template, 'html'); //default uses .template file ext for templates
+		this.use('Storage');
+		this.use('Session');
+		this.use('Title');
+		this.use(Sammy.JSON);
+
+		// HOMEPAGE
+		this.get('#!/', function (context) {
+			
+			
+		}); //end "get #/"
     
 		
 		
+		this.put('#/post/q', function(context) {
+			sammy.trigger('show-page', {page: 'links'});
+			alert(sammy.cities);
+			str = this.params['q'];
+			alert(str);
+			
+			var matchingItems = [];
+		  $.each(sammy.cities, function(key, val) {
+				cityname = val['ci'];
+				country = val['co'];
+				
+				if(str == cityname){
+					//perfect match!
+				}else{ 
+					//indexof...
+				}
+		    matchingItems.push(val['ci']); //city name
+		  });
+			
+			
+			if( perfectMatch( str ) ) {	//check if there's a perfect matching city / zone name
+				//sammy.trigger('add-url', {url: str});//add URL
+				//this.title('added: "'+str+'"');
+			}else{
+				// this.title('filter: '+str);
+				// sammy.trigger('filter-item');
+			}
+			return false; //event.preventDefault(); 
+		});
 		
 		
-});
+		
+	});//eo sammy routes
+		
+		sammy.run('#/');
+		
+}); //eo doc ready
