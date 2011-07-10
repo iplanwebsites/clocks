f9a786274b8951168a013c587aefe6097899fd7d
I Plan Websites <info@iplanwebsites.com>
2011-07-10 20:35:15 +0200

    Added more object manipulation to add new clocks

--- public/clock.js
+++ public/clock.js
@@ -27,6 +27,10 @@ $(document).ready(function() {
 	
 	
 	//Load Json - init...
+	$.getJSON('data/timezones.json', function(data) { //cached...
+			sammy.tz = data;  
+	});
+	
 	$.getJSON('data/cities.json', function(data) { //cached...
 		sammy.cities = data;  
 		var autocompleteItem = [];
@@ -34,19 +38,18 @@ $(document).ready(function() {
 	    autocompleteItem.push(val['ci']); //city name
 			autocompleteItem.push(val['z']); //zone name
 	  });
-
-		
 		autocompleteItem = removeDuplicateElement(autocompleteItem);
 		
-// We init the auto-complete once we gathered all city + zones names.
-$("input#q").autocomplete({
-	source: autocompleteItem,
-	minLength: 1,
-	delay: 0
-});
-
+		// We init the auto-complete once we gathered all city + zones names.
+		$("input#q").autocomplete({
+			source: autocompleteItem,
+			minLength: 1,
+			delay: 0
+		});
 	});// eo get Json (init)
 	
+	
+
 	
 	
 	
@@ -59,40 +62,69 @@ $("input#q").autocomplete({
 		this.use(Sammy.JSON);
 
 		// HOMEPAGE
-		this.get('#!/', function (context) {
+		this.get('/', function (context) {
 			
 			
 		}); //end "get #/"
     
 		
 		
-		this.put('#/post/q', function(context) {
+		this.post('#/post/q', function(context) {
 			sammy.trigger('show-page', {page: 'links'});
-			alert(sammy.cities);
+			// alert(sammy.cities);
 			str = this.params['q'];
-			alert(str);
+		//	alert(str);
 			
 			var matchingItems = [];
 		  $.each(sammy.cities, function(key, val) {
 				cityname = val['ci'];
 				country = val['co'];
 				
-				if(str == cityname){
+				newCity = 0;
+				possibleCities = [];
+				if(str == cityname){ //todo, lowercase the comparaison, replace the dashes...
 					//perfect match!
-				}else{ 
-					//indexof...
+					//alert("perfect!" + val);
+					newCity = val;
+				}else if(0){ //we find if it may be a possible match
+					if(str){	//indexof, without coma...
+						possibleCities.push(val); //!!TOOD correct it
+					}
+				}else{
+					//else we display the found options...
+					//let the user pick...?
 				}
-		    matchingItems.push(val['ci']); //city name
-		  });
+			});//eo cities for each
 			
+				if(newCity){ //flag: if we found a good match, good enought to add it without further disambiguation...
+					//We find which TZ this city relates to, and add it to the object.
+					 $.each(sammy.cities, function(index_tz, data_tz) {
+							if(data_tz['name'] == newCity['z']){
+								newCity['tz'] = data_tz; //we embed the timezone object within the city one
+							}
+					});//eo each tz
+					
+					context.clocksDiv = context.$element('#clocks');
+					//$(context.linkContainer).html('');
+          context.render('templates/clock.html', {city: newCity, tz: myTz)
+            .appendTo(context.clocksDiv).then(function(content) {
+							//init this clock's buttons
+							//context.trigger('filter-item'); //if field is already populated (page refresh)
+        	});
+					
+					
+				}
+		    //matchingItems.push(val['ci']); //city name
+		 
 			
+			/*
 			if( perfectMatch( str ) ) {	//check if there's a perfect matching city / zone name
 				//sammy.trigger('add-url', {url: str});//add URL
 				//this.title('added: "'+str+'"');
 			}else{
 				// this.title('filter: '+str);
 				// sammy.trigger('filter-item');
-			}
+			}*/
 			return false; //event.preventDefault(); 
 		});
 		
@@ -100,6 +132,6 @@ $("input#q").autocomplete({
 		
 	});//eo sammy routes
 		
-		sammy.run('#/');
+		sammy.run('/');
 		
 }); //eo doc ready
