d4e8476efe728262fee351f315842928072f2238
I Plan Websites <info@iplanwebsites.com>
2011-07-10 20:41:59 +0200

    added TZ data to selected city.

--- public/clock.js
+++ public/clock.js
@@ -75,16 +75,17 @@ $(document).ready(function() {
 			str = this.params['q'];
 		//	alert(str);
 			
-			var matchingItems = [];
+		//	var matchingItems = [];
+			newCity = 0;
+			possibleCities = [];
 		  $.each(sammy.cities, function(key, val) {
 				cityname = val['ci'];
 				country = val['co'];
 				
-				newCity = 0;
-				possibleCities = [];
+				
 				if(str == cityname){ //todo, lowercase the comparaison, replace the dashes...
 					//perfect match!
-					//alert("perfect!" + val);
+					alert("perfect!" + val);
 					newCity = val;
 				}else if(0){ //we find if it may be a possible match
 					if(str){	//indexof, without coma...
@@ -98,15 +99,17 @@ $(document).ready(function() {
 			
 				if(newCity){ //flag: if we found a good match, good enought to add it without further disambiguation...
 					//We find which TZ this city relates to, and add it to the object.
-					 $.each(sammy.cities, function(index_tz, data_tz) {
-							if(data_tz['name'] == newCity['z']){
+					
+					 $.each(sammy.tz, function(index_tz, data_tz) {
+							if(data_tz['name'] == newCity['z']){ 
+								alert('name matches!' + newCity);
 								newCity['tz'] = data_tz; //we embed the timezone object within the city one
 							}
 					});//eo each tz
 					
 					context.clocksDiv = context.$element('#clocks');
 					//$(context.linkContainer).html('');
-          context.render('templates/clock.html', {city: newCity, tz: myTz)
+          context.render('templates/clock.html', {city: newCity})
             .appendTo(context.clocksDiv).then(function(content) {
 							//init this clock's buttons
 							//context.trigger('filter-item'); //if field is already populated (page refresh)
