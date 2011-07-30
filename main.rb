81315269db9ca37c2b98f3e4288829ce1e8ac5dc
I Plan Websites <info@iplanwebsites.com>
2011-07-30 11:49:35 -0400

    added GMT/UTC route for server-sync

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'json'
 
 configure do
 	Rack::Mime::MIME_TYPES[".manifest"] = "text/cache-manifest"
@@ -8,4 +9,12 @@ end
 get '/' do
 	# redirect '/clock.html'
 	erb :index
+end
+
+
+get '/api/utc' do
+	t = Time.now 
+  "hello"
+  content_type 'text/html', :charset => 'utf-8'
+  t.utc.to_json
 end
